9b4910c9404dde9b708f9d153b16cdc51239b9f6
Michał <michal.lesnik92@gmail.com>
2025-06-10 19:24:29 +0200

    Update lib.js
    Removed test code and put something actually useful

--- lib.js
+++ lib.js
@@ -1,2 +1,7 @@
-let test_var = "works";
-console.log('lmao');
+function log_msg(msg) {
+  GM_xmlhttpRequest({
+    method: "POST",
+    url: "https://mlesnik.asuscomm.com:5000/log",
+    data: "msg=" + msg
+  });
+}
